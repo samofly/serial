dd7d5428e796ca3671167142b8467af092030012
Ivan Krasin <imkrasin@gmail.com>
2014-04-07 05:30:33 +0000

    Support all standard baud rates from B50 to B4000000

--- serial.go
+++ serial.go
@@ -63,14 +63,46 @@ func (p *port) Write(buf []byte) (int, error) { return p.f.Write(buf) }
 // Close implements io.Closer
 func (p *port) Close() error { return p.f.Close() }
 
+var knownRates = map[int]uint32{
+	50:      B50,
+	75:      B75,
+	110:     B110,
+	134:     B134,
+	150:     B150,
+	200:     B200,
+	300:     B300,
+	600:     B600,
+	1200:    B1200,
+	1800:    B1800,
+	2400:    B2400,
+	4800:    B4800,
+	9600:    B9600,
+	19200:   B19200,
+	38400:   B38400,
+	57600:   B57600,
+	115200:  B115200,
+	230400:  B230400,
+	460800:  B460800,
+	500000:  B500000,
+	576000:  B576000,
+	921600:  B921600,
+	1000000: B1000000,
+	1152000: B1152000,
+	1500000: B1500000,
+	2000000: B2000000,
+	2500000: B2500000,
+	3000000: B3000000,
+	3500000: B3500000,
+	4000000: B4000000,
+}
+
 // convRate converts numerical rate into the baud rate code, like B115200.
 func convRate(baud int) (uint32, error) {
-	switch baud {
-	case 115200:
-		return B115200, nil
-	default:
+	v, ok := knownRates[baud]
+	if !ok {
 		return 0, fmt.Errorf("unsupported baud rate: %v", baud)
 	}
+	return v, nil
 }
 
 // termios is a low-level structure that Linux kernel will understand.
